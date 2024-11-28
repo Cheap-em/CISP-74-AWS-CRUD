077266065a4277949577c3e4a348b7a0fc159159
HPOA909 <howkertree@icloud.com>
2024-11-27 18:23:33 -0800

    Other shenanigans

--- src/app.py
+++ src/app.py
@@ -3,6 +3,9 @@ import sqlite3 as sql
 from flask import *
 import sqlalchemy as aql
 
+# Insert some veriables for the program to follow below
+TODO: # Create some variables for the program to read
+
 # Insert function modules below
 TODO: # Create some define function modules for the classes below
 
