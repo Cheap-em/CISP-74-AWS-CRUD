33ab879ee3873ea726684c60d33e343a8cd5f6e5
HPOA909 <howkertree@icloud.com>
2024-11-27 18:20:47 -0800

    Creating a file for preparation

---
+++ src/app.py
@@ -0,0 +1,11 @@
+# Import some libraries in order to let the program to work
+import sqlite3 as sql
+from flask import *
+import sqlalchemy as aql
+
+# Insert function modules below
+TODO: # Create some define function modules for the classes below
+
+# Insert classes below
+TODO: # Create the classes via the functions above for shortcuts
+
