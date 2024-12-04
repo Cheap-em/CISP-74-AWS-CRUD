873289678ded86263047e00fc44c151715560832
HPOA909 <howkertree@icloud.com>
2024-12-03 22:10:39 -0800

    hmmmmmmmmmmm

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'your_secret_key'
+    SECRET_KEY = 'hmmmmmmmmmmmmmmmmmmmmmmmmm'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')  # Use MySQL URI for MySQL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
