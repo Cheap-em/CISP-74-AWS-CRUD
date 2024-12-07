c6ca0d3d625db19222c52fa3c84a8b05c6f89b26
HPOA909 <howkertree@icloud.com>
2024-12-07 11:21:07 -0800

    Bruhh

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from app import db, bcrypt
-from forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm
 from models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
